2fd00be946b659ebc9ee3f0a7fcd0df688702c9e
luizalvesot <alvesluizotavio23@gmail.com>
2023-08-21 15:51:05 -0300

    Ajustes nas rotas

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+Route::get('/', function(){
+    return redirect()->route('login');
+});
+
+Route::get('/register', function(){
+    return redirect()->route('register');
+});
+
+Route::fallback((function(){
+    return redirect()->route('home');
+}));
