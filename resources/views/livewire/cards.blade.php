2509b0f6890f9ff493afa366a0ae8ec9031fcd7d
luizalvesot <alvesluizotavio23@gmail.com>
2023-08-25 13:55:19 -0300

    Ajuste nos cartões

--- resources/views/livewire/cards.blade.php
+++ resources/views/livewire/cards.blade.php
@@ -10,7 +10,8 @@
             <div class="col-lg-3">
                 <div class="card my-2">
                     <div class="card-body">
-                        <strong class="text-center font-weight-bold m-4">Saídas <i class="mx-4 bi bi-arrow-down-circle-fill"></i></strong>
+                        <strong class="text-center font-weight-bold m-4">Saídas <i class="mx-5 px-4 bi bi-arrow-down-circle-fill"></i></strong>
+                        <h3 class="font-weight-bolder mr-5">R$ 200.00</h3>
                     </div>
                 </div>
             </div>
